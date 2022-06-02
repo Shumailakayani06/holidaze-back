370dd9d5de3d5cd47cbde88a4355539b2e00883b
Shumaila Kayani <shumailakayani059@gmail.com>
2022-06-02 10:58:24 +0200

    fixed database.js and server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,13 @@
 module.exports = ({ env }) => ({
-    url: env('MY_HEROKU_URL'),
-});
+
+    url: env(process.env.DATABASE_URL),
+    
+    proxy: true,
+    
+    app: {
+    
+    keys: env.array("APP_KEYS", ["KEY1", "KEY2"]),
+    
+    },
+    
+    });
