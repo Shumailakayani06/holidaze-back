dd710c20f3de72a27e42d187a4e824e91fd89e87
Shumaila Kayani <shumailakayani059@gmail.com>
2022-06-03 10:30:09 +0200

    jwt secret

--- config/plugins.js
+++ config/plugins.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
     'users-permissions': {
       config: {
         jwt: {
+          jwtSecret: env('JWT_SECRET'),
           expiresIn: '7d',
         },
       },
